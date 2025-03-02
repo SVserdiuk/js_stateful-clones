748448952b7855549cfdf0a1b238813f3887b8ac
Serhii Serdiuk <serhii.serdiuk@gmail.com>
2025-03-01 23:54:36 -0330

    Add function

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,28 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  let result = [];
+  let currentState = { ... state};
+  for (const obj of actions) {
+    if (obj.type === 'clear') {
+      currentState = {};
+      result.push({ ... currentState});
+    }
+
+    if (obj.type === 'addProperties') {
+      currentState = { ... currentState, ... obj.extraData};
+      result.push({ ... currentState});
+    }
+
+    if (obj.type === 'removeProperties') {
+      for (const x of obj.keysToRemove) {
+        delete currentState[x];
+      }
+      result.push({ ... currentState});
+    }
+  }
+
+  return result;
 }
 
 module.exports = transformStateWithClones;
